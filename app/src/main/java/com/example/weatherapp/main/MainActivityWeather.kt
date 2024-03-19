cefaea7996b26aac32d34d1a551a98b636af06ae
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2024-03-20 02:23:54 +0330

    Clean Code. Add Application class in project. Add package main . change package MainActivityWeather.kt .

--- app/src/main/java/com/example/weatherapp/main/MainActivityWeather.kt
+++ app/src/main/java/com/example/weatherapp/main/MainActivityWeather.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapp.ui.home.activity
+package com.example.weatherapp.main
 
 import android.content.pm.PackageManager
 import android.os.Bundle
---
+++ app/src/main/java/com/example/weatherapp/main/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.main
+
+import android.app.Application
+
+class WeatherApplication: Application() {
+
+}
